5e81576cde2d6de3ed8b3607578238e0f1ac2596
Samuel A. Barnett <sambarnett95@gmail.com>
2021-07-06 14:42:18 -0400

    sr-ps written, memory comparison exp added

--- neuronav/utils.py
+++ neuronav/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.random as npr
 
 def onehot(value, max_value):
     vec = np.zeros(max_value)
@@ -19,3 +20,41 @@ def mask_grid(grid, blocks, mask_value=-100):
                 grid[i,j] = mask_value
     grid = np.ma.masked_where(grid == mask_value, grid)
     return grid
+
+def exp_normalize(x):
+    b = x.max()
+    y = np.exp(x - b)
+    return y / y.sum()
+
+# memory utils
+
+def memory_update(exp, agent, epsilon, beta):
+    exp1 = exp.copy()
+    if not exp[-1]:
+        # change to "best" action in hindsight
+        exp1[1] = agent.sample_action(exp[0], epsilon=epsilon, beta=beta)
+    td_sr = agent.update_sr(exp, exp1)
+    return td_sr
+
+def get_dyna_indices(experiences, weights, nsamples):
+    p = exp_normalize(np.array(weights))
+    p /= p.sum()
+    return npr.choice(len(experiences), nsamples, p=p, replace=True)
+
+def get_predecessors(state, experiences):
+    return [exp for exp in experiences if (exp[2] == state)]
+
+def queue_append(exp, priority, queue):
+    
+    already_in_queue = False
+    # if exp is already in queue with a lower priority, replace with higher priority
+    for mem in queue:
+        if (mem["exp"] == exp):
+            already_in_queue = True
+            if (mem["priority"] < priority):
+                mem["priority"] = priority
+                                        
+    if not already_in_queue:
+        queue.append({"exp": exp, "priority": priority})
+                                                    
+    return queue
